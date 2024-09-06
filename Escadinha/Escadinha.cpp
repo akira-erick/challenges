70b7e9803ea4423c2f9decc1de326c1db9f9caa1
akira-erick <kajitani.motta@gmail.com>
2024-09-06 17:02:07 -0300

    Add Ladder

---
+++ Escadinha/Escadinha.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    
+    vector<int> numbers;
+
+    int times; cin >> times;
+    int number; 
+    while(times--){
+        cin >> number;
+        numbers.push_back(number);
+    }
+
+    int dif;
+
+    
+
+    return 1;
+}
